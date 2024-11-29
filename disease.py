aa3d414db2b2d4477136375f488daebf3111b1ce
PhilipVickery <pvickery@vols.utk.edu>
2024-11-29 17:14:17 -0600

    user input
    added user input and prediction, with output of if the user has a high or low chance of developing coronary heart disease

--- disease.py
+++ disease.py
@@ -20,3 +20,36 @@ from sklearn.tree import DecisionTreeClassifier
 dt = DecisionTreeClassifier()
 dt.fit(x_train, y_train)
 print("score: ", dt.score(x_test, y_test))
+
+#ask user to input information
+userInput = []
+iName = input("Enter name: ")
+userInput.append(int(input("M or F? (Enter 1 or 0): ")))
+userInput.append(int(input("Enter age: ")))
+userInput.append(int(input("Enter education level (1-4): ")))
+userInput.append(int(input("Are you a current smoker? Enter 1 or 0: ")))
+userInput.append(int(input("How many cigs do you smoke per day? ")))
+userInput.append(int(input("Are you on blood pressure meds? (enter 1 or 0): ")))
+userInput.append(int(input("Do you have prevalent strokes? (enter 1 or 0): ")))
+userInput.append(int(input("Do you have prevalent hypertension? (enter 1 or 0): ")))
+userInput.append(int(input("Do you have diabetes? (enter 1 or 0): ")))
+userInput.append(float(input("What is your total cholesterol? (50-400): ")))
+userInput.append(float(input("What is your systolic blood pressure? (50-200): ")))
+userInput.append(float(input("What is your diastolic blood pressure? (40-200): ")))
+userInput.append(float(input("What is your body mass index? (15-50): ")))
+userInput.append(float(input("What is your heart rate? (20-200): ")))
+userInput.append(float(input("What is your glucose level? (50-200): ")))
+
+#convert user input to a DataFrame
+userData = pd.DataFrame([userInput], columns = x_data.columns)
+
+#normalize userData
+userX = (userData - np.min(x_data)) / (np.max(x_data) - np.min(x_data))
+
+#make prediction and output result
+print("Predicting chances of coronary heart disease within the next 10 years...\n")
+prediction = dt.predict(userX)
+if prediction[0] == 1:
+    print("You have a high chance of coronary heart disease.")
+else:
+    print("You have a low chance of coronary heart disease.")
